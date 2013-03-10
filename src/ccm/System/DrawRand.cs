4f110410f84cfabe946c4533963de44f59d677a2
himapo <datalocal@gmail.com>
2013-03-10 21:43:13 +0900

    ファイル追加忘れ

---
+++ src/ccm/System/DrawRand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HimaLib.Math;
+
+namespace ccm.System
+{
+    public class DrawRand : HimaLib.Math.SystemRand
+    {
+        static readonly DrawRand instance = new DrawRand();
+
+        public static DrawRand Instance { get { return instance; } private set { } }
+
+        DrawRand()
+        {
+        }
+    }
+}
---
+++ src/ccm/System/GameRand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HimaLib.Math;
+
+namespace ccm.System
+{
+    public class GameRand : HimaLib.Math.SystemRand
+    {
+        static readonly GameRand instance = new GameRand();
+
+        public static GameRand Instance { get { return instance; } private set { } }
+
+        GameRand()
+        {
+        }
+    }
+}
