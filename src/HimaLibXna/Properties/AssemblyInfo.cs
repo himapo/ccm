2460c55953e871c2dc2c2c9ff27808d09f36f43a
himapo <datalocal@gmail.com>
2013-07-13 20:13:40 +0900

    HimaLibXnaを普通のクラスライブラリからXNAゲームライブラリに変更
    GraphicsProfileをHiDefにしてもReachになってしまう不具合の対応のため

--- src/HimaLibXna/Properties/AssemblyInfo.cs
+++ src/HimaLibXna/Properties/AssemblyInfo.cs
@@ -1,36 +1,34 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
-// アセンブリに関連付けられている情報を変更するには、
-// これらの属性値を変更してください。
+// アセンブリに関する全般的な情報は、以下の一連の属性によって管理されます。 
+// アセンブリに関連付けられている情報を変更するには、これらの属性値を変更します。
+// 
 [assembly: AssemblyTitle("HimaLibXna")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HimaLibXna")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
-// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
-// その型の ComVisible 属性を true に設定してください。
+// ComVisible 属性を False に設定すると、このアセンブリに含まれる型が 
+// COM コンポーネントに公開されません。COM からこのアセンブリの型にアクセスする必要がある場合は、 
+// その型の ComVisible 属性を True に設定してください。
+// Windows アセンブリだけが COM に対応しています。
 [assembly: ComVisible(false)]
 
-// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
-[assembly: Guid("e1499a90-80f7-465a-a9e0-0f0d9aaff5e4")]
+// Windows 上では、このプロジェクトが COM に公開されている場合、 次の GUID が typelib の ID として設定されます。
+// Windows 以外のプラットフォームでは、このアセンブリをデバイスに展開するときに
+// タイトル ストレージ コンテナーを一意に識別するために使用します。
+[assembly: Guid("59fe3671-3e8b-4339-969b-bc884fc09f96")]
 
-// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+// アセンブリのバージョン情報は、次の 4 つの値で構成されています。
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+//      メジャー バージョン
+//      マイナー バージョン 
+//      ビルド番号
+//      リビジョン番号
 //
-// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
-// 既定値にすることができます:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
