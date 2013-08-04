4dfcb271175bf821599023c48f6fb6191034b102
himapo <datalocal@gmail.com>
2013-08-04 21:13:12 +0900

    フォント処理をWPFフォントプロセッサに移行
    http://blogs.msdn.com/b/ito/archive/2012/02/19/wpf-font-processor.aspx

--- src/ccm/Scene/BootScene.cs
+++ src/ccm/Scene/BootScene.cs
@@ -28,7 +28,7 @@ namespace ccm.Scene
 
             Name = "BootScene";
 
-            debugMenu = new DebugMenu("ブートメニュー");
+            debugMenu = new DebugMenu("起動メニュー");
             debugMenuUpdater = new DebugMenuUpdater(debugMenu);
             debugMenuDrawer = new DefaultDebugMenuDrawer();
         }
@@ -107,7 +107,7 @@ namespace ccm.Scene
 
             DebugFont.Add(Name, 50.0f, 60.0f);
 
-            DebugFont.Add("あいうえお", 200.0f, 20.0f);
+            DebugFont.Add("●「記号いける」", 200.0f, 20.0f);
 
             ShowMousePosition();
         }
