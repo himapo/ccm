07f6e4d6e994da18e225bac29866944e8d36f490
himapo <datalocal@gmail.com>
2013-10-19 16:12:12 +0900

    インスタンシングモデルを視錐台に外接するAABBでカリングするようにした

--- src/HimaLibXna/Render/SimpleInstancingRendererXna.cs
+++ src/HimaLibXna/Render/SimpleInstancingRendererXna.cs
@@ -15,6 +15,16 @@ namespace HimaLib.Render
 
         Microsoft.Xna.Framework.Matrix[] ModelBones;
 
+        /// <summary>
+        /// 視錐台を覆うAABBのXYZそれぞれの最大座標
+        /// </summary>
+        Vector3 MinFrustumAABB;
+
+        /// <summary>
+        /// 視錐台を覆うAABBのXYZそれぞれの最小座標
+        /// </summary>
+        Vector3 MaxFrustumAABB;
+
         public SimpleInstancingRendererXna()
         {
         }
@@ -27,12 +37,17 @@ namespace HimaLib.Render
                 return;
             }
 
-            if (param.TransformsUpdated)
-            {
-                SetInstanceTransforms(param.Transforms, param.Camera);
-            }
+            UpdateFrustum(param.Camera);
 
-            shader.TransformsUpdated = param.TransformsUpdated;
+            shader.InstanceTransforms = param.Transforms.Where(t =>
+            {
+                return FrustumCulling(t, 3.0f);
+            }).Select(t =>
+            {
+                return MathUtilXna.ToXnaMatrix(t.WorldMatrix);
+            }).ToArray();
+
+            shader.TransformsUpdated = true;
             shader.View = MathUtilXna.ToXnaMatrix(param.Camera.View);
             shader.Projection = MathUtilXna.ToXnaMatrix(param.Camera.Projection);
             shader.AmbientLightColor = MathUtilXna.ToXnaVector(param.AmbientLightColor);
@@ -44,19 +59,63 @@ namespace HimaLib.Render
             shader.ShadowMap = (param.ShadowMap as ITextureXna).Texture;
         }
 
-        void SetInstanceTransforms(List<AffineTransform> transforms, CameraBase camera)
+        void UpdateFrustum(CameraBase camera)
         {
-            shader.InstanceTransforms = transforms.Where(t =>
+            var invViewProj = Matrix.Invert(camera.View * camera.Projection);
+
+            var projCorners = new Vector3[8]
             {
-                return FrustumCulling(camera, t, 3.0f);
-            }).Select(t =>
+                new Vector3(-1, 1, 0),
+                new Vector3(1, 1, 0),
+                new Vector3(-1, -1, 0),
+                new Vector3(1, -1, 0),
+                new Vector3(-1, 1, 1),
+                new Vector3(1, 1, 1),
+                new Vector3(-1, -1, 1),
+                new Vector3(1, -1, 1),
+            };
+
+            var worldCorners = projCorners.Select(c =>
             {
-                return MathUtilXna.ToXnaMatrix(t.WorldMatrix);
-            }).ToArray();
+                return Vector3.TransformCoord(c, invViewProj);
+            });
+
+            // 計算を軽くするため、視錐台に外接するAABBでカリングする
+            MinFrustumAABB = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+            MaxFrustumAABB = new Vector3(float.MinValue, float.MinValue, float.MinValue);
+
+            foreach (var frustumCorner in worldCorners)
+            {
+                MinFrustumAABB.X = MathUtil.Min(MinFrustumAABB.X, frustumCorner.X);
+                MinFrustumAABB.Y = MathUtil.Min(MinFrustumAABB.Y, frustumCorner.Y);
+                MinFrustumAABB.Z = MathUtil.Min(MinFrustumAABB.Z, frustumCorner.Z);
+
+                MaxFrustumAABB.X = MathUtil.Max(MaxFrustumAABB.X, frustumCorner.X);
+                MaxFrustumAABB.Y = MathUtil.Max(MaxFrustumAABB.Y, frustumCorner.Y);
+                MaxFrustumAABB.Z = MathUtil.Max(MaxFrustumAABB.Z, frustumCorner.Z);
+            }
         }
 
-        bool FrustumCulling(CameraBase camera, AffineTransform transform, float margin)
+        bool FrustumCulling(AffineTransform transform, float margin)
         {
+            if (transform.Translation.X > MaxFrustumAABB.X + margin)
+                return false;
+
+            if (transform.Translation.Y > MaxFrustumAABB.Y + margin)
+                return false;
+
+            if (transform.Translation.Z > MaxFrustumAABB.Z + margin)
+                return false;
+
+            if (transform.Translation.X < MinFrustumAABB.X - margin)
+                return false;
+
+            if (transform.Translation.Y < MinFrustumAABB.Y - margin)
+                return false;
+
+            if (transform.Translation.Z < MinFrustumAABB.Z - margin)
+                return false;
+
             return true;
         }
 
